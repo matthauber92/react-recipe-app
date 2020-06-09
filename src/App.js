df6fef77e07909fcc402123fa7d75e1f438a93f8
Matthew Hauber <mhauber@lincolnlearningsolutions.org>
2020-06-09 15:58:38 -0400

    add react app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,61 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import Recipe from './Recipe';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
+const App = () => {
+
+  const APP_ID = '79235c2a';
+  const APP_KEY = 'e83c51527bb28a81c864417d1c0d4792';
+
+  const [recipes, setRecipes] = useState([]);
+  const [search, setSearch] = useState('');
+  const [counter, setCounter] = useState(0);
+  const [query, setQuery] = useState('chicken');
+
+  useEffect(() => {
+    getRecipes();
+  }, [query]);
+
+  const getRecipes = async () => {
+    const response = await fetch('https://api.edamam.com/search?q=' + query + '&app_id=' + APP_ID + '&app_key=' + APP_KEY);
+    const data = await response.json();
+    setRecipes(data.hits);
+  }
+
+  const updateSearch = e => {
+    setSearch(e.target.value);
+  }
+
+  const getSearch = e => {
+    e.preventDefault();
+    setQuery(search);
+  }
+
+  return(
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={getSearch} className="search-form">
+        <input
+         className="search-bar"
+         type="text"
+         value={search}
+         onChange={updateSearch}/>
+
+        <button className="search-button" type="submit">
+        Search
+        </button>
+      </form>
+      <div className="recipes">
+        {recipes.map(recipe => (
+          <Recipe key={recipe.recipe.label} title={recipe.recipe.label} calories={recipe.recipe.calories}
+            ingredients={recipe.recipe.ingredients} image={recipe.recipe.image}
+           />
+        ))}
+      </div>
+       <h1 onClick={() => setCounter(counter + 1)}>{counter}</h1>
     </div>
   );
 }
+
 
 export default App;
